69e31d08ebe7b960ea36af51bcf7a32cd6bc1996
Ha Tien Thang <tienthang2298@gmail.com>
2019-12-18 15:55:02 +0700

    2019-12-18 (YYYY-MM-DD) Finish API GET and DELETE.

---
+++ Company/Controllers/StaffsController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Company.Models;
+
+namespace Company.Controllers
+{
+    public class StaffsController : ApiController
+    {
+        private CompanyEntities db = new CompanyEntities();
+
+        // GET: api/Staffs
+        public IQueryable<StaffDto> GetStaffs()
+        {
+            var staffs = from st in db.Staffs
+                         select new StaffDto()
+                         {
+                             StaffID = st.StaffID,
+                             StaffName = st.StaffName,
+                             StartWorkingAt = st.StartWorkingAt
+                         };
+            return staffs;
+            //return db.Staffs;
+        }
+
+        // GET: api/Staffs/5
+        [ResponseType(typeof(Staff))]
+        public IHttpActionResult GetStaff(long id)
+        {
+            Staff staff = db.Staffs.Find(id);            
+            if (staff == null)
+            {
+                return NotFound();
+            }
+            var staffDto = new StaffDto()
+            {
+                StaffName = staff.StaffName,
+                StaffID = staff.StaffID,
+                StartWorkingAt = staff.StartWorkingAt
+            };
+            return Ok(staff);
+        }
+
+        // PUT: api/Staffs/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutStaff(long id, Staff staff)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != staff.StaffID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(staff).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StaffExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Staffs
+        [ResponseType(typeof(Staff))]
+        public IHttpActionResult PostStaff(Staff staff)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Staffs.Add(staff);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = staff.StaffID }, staff);
+        }
+
+        // DELETE: api/Staffs/5
+        [ResponseType(typeof(Staff))]
+        public IHttpActionResult DeleteStaff(long id)
+        {
+            Staff staff = db.Staffs.Find(id);
+            if (staff == null)
+            {
+                return NotFound();
+            }
+
+            db.Staffs.Remove(staff);
+            db.SaveChanges();
+
+            return Ok(staff);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool StaffExists(long id)
+        {
+            return db.Staffs.Count(e => e.StaffID == id) > 0;
+        }
+    }
+}
---
+++ Company/Scripts/app.js
@@ -0,0 +1,49 @@
+﻿var ViewModel = function () {
+    var self = this;
+    self.staffs = ko.observableArray();
+    self.error = ko.observable();
+    self.detail = ko.observable();
+    self.isDelete = ko.observable();
+    self.isDelete(0);
+
+    var staffsUri = '/api/staffs/'
+
+    function ajaxHelper(uri, method, data) {
+        self.error(''); // Clear error
+        return $.ajax({
+            type: method,
+            url: uri,
+            dataType: 'json',
+            contentType: 'application/json',
+            data: data ? JSON.stringify(data) : null
+        }).fail(function (jqXHR, textStatus, errorThrown) {
+            self.error(errorThrown);
+        });
+    }
+    function getAllStaffs() {
+        ajaxHelper(staffsUri, 'GET').done(function (data) {
+            self.staffs(data);
+        });
+    }
+    self.getStaffByID = function (item) {
+        var del = document.getElementById('delete-notice');
+        if (del != null) { del.style.display = "none"; }
+        ajaxHelper(staffsUri + item.StaffID, 'GET').done(function (data) {
+            self.detail(data);
+        });
+        var detail = document.getElementById('detail-notice');
+        if (detail != null) { detail.style.display = "block"; } 
+    }
+    self.deleteStaffByID = function (item) {
+        var detail = document.getElementById('detail-notice');
+        if (detail != null) { detail.style.display = "none"; } 
+        ajaxHelper(staffsUri + item.StaffID, 'DELETE').done(function (data) {
+            getAllStaffs();
+            self.isDelete(1);
+        });
+        var del = document.getElementById('delete-notice');
+        if (del != null) { del.style.display = "block"; }
+    }
+    getAllStaffs();
+};
+ko.applyBindings(new ViewModel());
